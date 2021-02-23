72aeb58f869cb11cb86e5406ba7d63d47c79ed7e
Sam Kim <sendtosamkim@gmail.com>
2021-02-22 22:16:26 -0500

    able to save to notes to server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// DEPENDENCIES
+// DEPENDENCIES----------------------------
 const express = require('express');
 const app = express(); // tell node we're creating express server
 const fs = require('fs'); //file sharing
@@ -11,20 +11,22 @@ const database = './data/db.json'; // wire up the saved data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// DATA
+// DATA----------------------------
 let noteArray = fs.existsSync(database) ?
     JSON.parse(fs.readFileSync(database)) : [] // parse the existing JSON; if not, create an array
+    console.log('noteArray1', noteArray);
 
-// ROUTES
+// API ROUTES----------------------------
 function processingFunction(req, res) {
+    console.log('noteArray2', noteArray)
     res.send(noteArray)
-}
-app.get('/api/notes', processingFunction) //find the notes list in the path
+};
+app.get('/api/notes', processingFunction); //find the notes list in the path
 // Push to JSON data array new input
 app.post('/api/notes', function (req, res) {
-    console.log('[POST /api/notes]', req.body);
+    console.log('[POSTING TO /api/notes]', req.body);
     const newNote = req.body;
-    console.log(`noteArray (Entry number: ${noteArray.length}), adding newNote: `, newNote);
+    console.log(`noteArray (Entry number: ${noteArray.length+1}), adding newNote: `, newNote);
     noteArray.push(newNote);
     // save to a file, as a string like localStorage
     fs.writeFileSync(database, JSON.stringify(noteArray))
@@ -32,8 +34,8 @@ app.post('/api/notes', function (req, res) {
     res.send({ message: `Reserved for *${newNote.name}*` })
 });
 
-// route to notes.html
-app.get('/notes', (req, res) => {
+// HTML ROUTES----------------------------
+app.get('/notes/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 // if no matching route is found, then default to index.html
@@ -41,7 +43,7 @@ app.get(['/index', '*'], (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// LISTENER
+// LISTENER----------------------------
 app.listen(PORT, () => {
     console.log('Listening on PORT: ', PORT);
-})
+});
