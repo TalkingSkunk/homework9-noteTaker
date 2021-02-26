3bd60c63487b93886048225202e8a9b8071d8a45
Sam Kim <sendtosamkim@gmail.com>
2021-02-25 20:44:32 -0500

    revamped entire files, and starting over

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ const express = require('express');
 const app = express(); // tell node we're creating express server
 const fs = require('fs'); //file sharing
 const path = require('path');
+const uuid=require('node-uuid')
 
 const PORT = process.env.PORT || 3000; // scan initial port
 const database = './data/db.json'; // wire up the saved data
 
+// will share any static html files with the browser
+app.use(express.static('public'));
+
 // handle data parsing for POST request
+// accept incoming POST requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 
 // DATA----------------------------
 let noteArray = fs.existsSync(database) ?
@@ -26,6 +33,8 @@ app.get('/api/notes', processingFunction); //find the notes list in the path
 app.post('/api/notes', function (req, res) {
     console.log('[POSTING TO /api/notes]', req.body);
     const newNote = req.body;
+    //{title:,text:,id:}
+    newNote.id=uuid()
     console.log(`noteArray (Entry number: ${noteArray.length+1}), adding newNote: `, newNote);
     noteArray.push(newNote);
     // save to a file, as a string like localStorage
@@ -34,16 +43,22 @@ app.post('/api/notes', function (req, res) {
     res.send({ message: `Reserved for *${newNote.name}*` })
 });
 
-// HTML ROUTES----------------------------
-app.get('/notes/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-// if no matching route is found, then default to index.html
-app.get(['/index', '*'], (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// delete an entry from db.json
+app.delete ( 'api/notes/:id', (req,res)=>{
+    const noteArrayId = getIndexById( req.params.id, noteArray);
+    if (noteArrayId !== -1) {
+        noteArray.splice (noteArrayId, 1);
+        re.status (204).send();
+    } else {
+        res.status (404).send();
+    }
+})
 
 // LISTENER----------------------------
 app.listen(PORT, () => {
     console.log('Listening on PORT: ', PORT);
 });
+
+
+
+let noteList = [{id: "0000-0000-0000-0000", title: 'note1', text: 'note1 text'}];
